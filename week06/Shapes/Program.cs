da4eae0afa9267acfce6060bb09431701564c450
angellaajulu <angellaevelyna@gmail.com>
2025-12-05 15:14:15 +0300

    completed shapes program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 5, 3));
+        shapes.Add(new Circle("Green", 2.5));
+
+        // Display areas using polymorphism
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Shape Color: {s.GetColor()}");
+            Console.WriteLine($"Area: {Math.Round(s.GetArea(), 2)}\n");
+        }
     }
 }
